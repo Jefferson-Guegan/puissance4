4dcb87f4140a2a6caaa239e30ffa9e72a7f73334
Administrateur <Jefferson-Guegan>
2022-03-31 16:24:03 +0200

    intégration css index

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -10,12 +10,27 @@ html="""
     <head>
         <meta charset="utf-8" />
         <title> Puissance 4 de la muerte</title>
-        <link rel="stylesheet" media="screen" type="text/css" href="style.css"/>
+        <link rel="stylesheet" media="screen" type="text/css" href="../style.css"/>
     </head>
 
     <body>
 
-    <h1>Bienvenue sur cette version réseau du célèbre jeu : Puissance 4<h1>
+        <h1>Bienvenue sur cette version réseau du célèbre jeu : Puissance 4</h1>
+
+        <form id="box" action="http://localhost:8000/cgi-bin/partie.py" method="post">
+            <legend> Lancer une partie de puissance 4 </legend>
+            <div id="container">
+                <div id="user">
+                    <label> Nom de guerre </label> <br>
+                    <input  type = "text" name = "userName"> <br>
+                </div>    
+                <div id="perso">    
+                    <label> Couleur </label>
+                    <input type = "text" name = "userColor">
+                </div>
+            </div>    
+            <input class="btn-grad" type="submit" value="C'est parti !">                  
+        </form>
     
     </body>
 </html>"""
