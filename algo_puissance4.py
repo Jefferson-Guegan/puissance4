392e09c0e8682246ec228d7603bd095a8731e78c
Goulve <goulven.thomas@outlook.fr>
2022-04-01 15:26:58 +0200

    Version stable algo Puissance4

--- algo_puissance4.py
+++ algo_puissance4.py
@@ -1,5 +1,8 @@
-
+################################ PUISSANCE 4 #################################
+from calendar import c
 from colors import Colors
+
+###### CLASSES ######
 
 # Définition du joueur
 class Joueur:
@@ -43,12 +46,13 @@ class Tableau:
                 chaine+=point+ " "
             print(chaine)
 
-            
-            
+
+###### MAIN ######            
+         
 colo=input("Quel couleur est votre jeton? : Y/R  ")
 Nom1=input("Quel est votre nom? :  ")
 
-if colo=="Y":
+if colo=="Y" or colo=="y" or colo=="":
     color1=Colors.YELLOW
     Joueur1=Joueur(color1,Nom1)
     Nom2=input("Joueur2 quel est votre nom? :  ")
@@ -71,32 +75,79 @@ tab.create_tab()
 
 rep=0
 
-count=0
+count_col=0
+count_lig=0
+victoire=False
 
-while count<=4:
+while victoire==False:
     
     for joueur in Joueurs:
-        count=0
-        print(count)
+        count_col=0
+        count_lig=0
+        count_diaga=0
+        count_diagd=0
         tab.affichage()
-        rep=int(input(joueur.nom+" : Dans quelle colonne voulez-vous placer votre pion : "))
-        j=5
-        while j>=0:     
-            if (tab.tab[j][rep-1] == "."):
-                tab.tab[j][rep-1]=joueur.jeton
-                Last_jeton=[j,rep-1]
-                j=-1
-            else:
-                j-=1
+        rep=int(input(joueur.nom+" : Dans quelle colonne voulez-vous placer votre pion (1-7): "))
+        
+        while rep<1 or rep>7:
+            rep=int(input(joueur.nom+" t'es débile... : ENTRE 1 ET 7 : "))
 
-        for i in range(7):
-            for j in range(5):
 
-                if tab.tab[j][i]==joueur.jeton and tab.tab[j+1][i]==joueur.jeton:
-                    count+=1              
-                
-        if count==3:
-            print("WIN!!")
+        if rep>0 and rep<8:
+            j=5
+            while j>=0:     
+                if (tab.tab[j][rep-1] == "."):
+                    tab.tab[j][rep-1]=joueur.jeton
+                    Last_jeton=[j,rep-1]
+                    j=-1
+                else:
+                    j-=1
+            # Condition victoire colonne
+            if Last_jeton[0]<3 and tab.tab[Last_jeton[0]+1][Last_jeton[1]]==joueur.jeton and tab.tab[Last_jeton[0]+2][Last_jeton[1]]==joueur.jeton and tab.tab[Last_jeton[0]+3][Last_jeton[1]]==joueur.jeton:
+                count_col=4
+            
+            # Conditions victoire lignes
+            elif 0<=Last_jeton[1]<4 and tab.tab[Last_jeton[0]][Last_jeton[1]+1]==joueur.jeton and tab.tab[Last_jeton[0]][Last_jeton[1]+2]==joueur.jeton and tab.tab[Last_jeton[0]][Last_jeton[1]+3]==joueur.jeton:
+                count_lig=4
+            elif Last_jeton[1]>2 and tab.tab[Last_jeton[0]][Last_jeton[1]-1]==joueur.jeton and tab.tab[Last_jeton[0]][Last_jeton[1]-2]==joueur.jeton and tab.tab[Last_jeton[0]][Last_jeton[1]-3]==joueur.jeton:
+                count_lig=4
+            elif 0>Last_jeton[1] and Last_jeton[1]<5 and tab.tab[Last_jeton[0]][Last_jeton[1]-1]==joueur.jeton and tab.tab[Last_jeton[0]][Last_jeton[1]+1]==joueur.jeton and tab.tab[Last_jeton[0]][Last_jeton[1]+2]==joueur.jeton:
+                count_lig=4
+            elif 1>Last_jeton[1] and Last_jeton[1]<6 and tab.tab[Last_jeton[0]][Last_jeton[1]-2]==joueur.jeton and tab.tab[Last_jeton[0]][Last_jeton[1]-1]==joueur.jeton and tab.tab[Last_jeton[0]][Last_jeton[1]+1]==joueur.jeton:
+                count_lig=4
+
+            # Conditions Diagonales ascendantes
+            
+            elif Last_jeton[0]>2 and Last_jeton[1]<4 and tab.tab[Last_jeton[0]-1][Last_jeton[1]+1]==joueur.jeton and tab.tab[Last_jeton[0]-2][Last_jeton[1]+2]==joueur.jeton and tab.tab[Last_jeton[0]-3][Last_jeton[1]+3]==joueur.jeton:
+                count_diaga=4 #diagonale 3 pions au dessus a droite
+            elif Last_jeton[0]>1 and Last_jeton[1]<5 and Last_jeton[0]<5 and Last_jeton[1]>0 and tab.tab[Last_jeton[0]-1][Last_jeton[1]+1]==joueur.jeton and tab.tab[Last_jeton[0]-2][Last_jeton[1]+2]==joueur.jeton and tab.tab[Last_jeton[0]+1][Last_jeton[1]-1]==joueur.jeton:
+                count_diaga=4 # diagonal 2 au dessus à droite, 1 en dessous à gauche
+            elif Last_jeton[0]<4 and Last_jeton[1]>0 and Last_jeton[0]<4 and Last_jeton[1]>1 and tab.tab[Last_jeton[0]-1][Last_jeton[1]+1]==joueur.jeton and tab.tab[Last_jeton[0]+1][Last_jeton[1]-1]==joueur.jeton and tab.tab[Last_jeton[0]+2][Last_jeton[1]-2]==joueur.jeton:
+                count_diaga=4 # diagonale 1 au dessus à droite, 2 en dessous à gauche
+            elif Last_jeton[0]<3 and Last_jeton[1]>2 and tab.tab[Last_jeton[0]+1][Last_jeton[1]-1]==joueur.jeton and tab.tab[Last_jeton[0]+2][Last_jeton[1]-2]==joueur.jeton and tab.tab[Last_jeton[0]+3][Last_jeton[1]-3]==joueur.jeton:
+                count_diaga=4 #diagonale 3 pions en dessous a droite
+
+            # Conditions Diagonales descendantes
+
+            elif Last_jeton[0]<3 and Last_jeton[1]<4 and tab.tab[Last_jeton[0]+1][Last_jeton[1]+1]==joueur.jeton and tab.tab[Last_jeton[0]+2][Last_jeton[1]+2]==joueur.jeton and tab.tab[Last_jeton[0]+3][Last_jeton[1]+3]==joueur.jeton:
+                count_diagd=4 #diagonale 3 pions en dessous a droite
+            elif Last_jeton[0]<4 and Last_jeton[1]<5 and Last_jeton[0]>0 and Last_jeton[1]>0 and tab.tab[Last_jeton[0]+1][Last_jeton[1]+1]==joueur.jeton and tab.tab[Last_jeton[0]+2][Last_jeton[1]+2]==joueur.jeton and tab.tab[Last_jeton[0]-1][Last_jeton[1]-1]==joueur.jeton:
+                count_diagd=4 # diagonale 2 en dessous à droite, 1 au dessus à gauche
+            elif Last_jeton[0]<5 and Last_jeton[1]>6 and Last_jeton[0]>1 and Last_jeton[1]>1 and tab.tab[Last_jeton[0]+1][Last_jeton[1]+1]==joueur.jeton and tab.tab[Last_jeton[0]-1][Last_jeton[1]-1]==joueur.jeton and tab.tab[Last_jeton[0]-2][Last_jeton[1]-2]==joueur.jeton:
+                count_diagd=4 # diagonal 1 en dessous à droite, 2 au dessus à gauche
+            elif Last_jeton[0]>2 and Last_jeton[1]>2 and tab.tab[Last_jeton[0]-1][Last_jeton[1]-1]==joueur.jeton and tab.tab[Last_jeton[0]-2][Last_jeton[1]-2]==joueur.jeton and tab.tab[Last_jeton[0]-3][Last_jeton[1]-3]==joueur.jeton:
+                count_diagd=4 #diagonale 3 pions au dessus a gauche
+
+            print(count_col,count_lig,count_diaga,count_diagd)     
+            if count_col==4 or count_lig==4 or count_diaga==4 or count_diagd==4:
+                victoire=True
+                print("WIN!!")
+                tab.affichage()
+                exit
+
+
+
+
             
                 
 
